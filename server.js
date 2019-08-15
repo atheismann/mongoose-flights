8c4bc22ca8b426163f7e084aedb4c428f46a3706
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-15 10:33:09 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const flightsRouter = require('./routes/flights');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use('/flights', flightsRouter);
 
 
 app.listen(port, () => console.log(`Express is listening on part ${port}`));
