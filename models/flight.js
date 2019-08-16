f683f63bad90ad2bbbd4d1896fc3f951b77d8a3d
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-15 21:29:01 -0500

    add destination schema

--- models/flight.js
+++ models/flight.js
@@ -3,17 +3,32 @@ const Schema = mongoose.Schema;
 const oneYear = 365 * 24 * 60 * 60 * 1000;
 const oneYearLater = () => { return new Date(Date.now() + oneYear) };
 
+const destinationSchema = new Schema({
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SEA'],
+    },
+    arrival: Date,
+})
+
 const flightSchema = new Schema({
     airline: String,
     flightNo: {
         type: Number,
         min: 10,
         max: 9999,
+        enum: ['American (AAL)', 'Southwest (WN)', 'United (UAL)'],
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DAL', 'LAX', 'SEA'],
+        default: 'SEA',
     },
     departs: {
         type: Date,
         default: oneYearLater,
-    }
+    },
+    destinations: destinationSchema,
 }, {
     timestamps: true
 })
