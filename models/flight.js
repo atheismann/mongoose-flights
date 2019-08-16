62723bc05fd93a4a504456928f059bc0043953ed
Andrew Theismann <andrew@theismannconsulting.com>
2019-08-15 20:01:59 -0500

    add flight model

---
+++ models/flight.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: String,
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: () =>
+            return new Date().now().getFullYear(+1);
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Flight', flightSchema);
